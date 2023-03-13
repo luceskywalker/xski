720dfa4ac43dfc06d32f59ed3824c3dcae088457
LH <lucas.hoeschler@plus.ac.at>
2023-03-13 20:15:23 +0100

    subtechnique distribution map almost works

---
+++ code/skripts/subtech.py
@@ -0,0 +1,63 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+from matplotlib.lines import Line2D
+import glob
+from pathlib import Path
+
+# path
+working_dir = Path().absolute().parent.parent/'files'
+files = glob.glob(str(working_dir) + r'/subtech*.csv')
+data_dir = Path(r'C:\Users\b1090197\Documents\Case Study Kit\Recordings')
+
+color_dict = {'dp': 'blue',
+              'diagonal': 'green',
+              'glide': 'red',
+              'glide-down': 'red'}
+custom_lines = [Line2D([0], [0], color=color_dict['dp'], lw=4),
+                Line2D([0], [0], color=color_dict['diagonal'], lw=4),
+                Line2D([0], [0], color=color_dict['glide'], lw=4)]
+# loop over all participants
+
+for i, trial in enumerate(files):
+    # get participant name and intensity
+    participant = Path(trial).name.split('_')[1]
+    intensity = Path(trial).name.split('_')[2][:-4]
+
+    # read csv
+    df = pd.read_csv(trial, sep=';')
+
+    gps = pd.read_excel(data_dir/participant/'MVNX'/(intensity + '_round.xlsx'), sheet_name='Global Position', index_col=0)
+    for k in range(len(df)-1):
+        plt.plot(gps.iloc[df['last frame'].iloc[k]:df['last frame'].iloc[k+1],0], gps.iloc[df['last frame'].iloc[k]:df['last frame'].iloc[k+1],1], color = color_dict[df['sub-technique'].iloc[k+1]])
+    plt.legend(custom_lines, list(color_dict.keys())[:-1])
+    plt.title('Subtechnique Distribution - ' + participant + ' ' + intensity)
+    plt.show()
+
+
+
+    # fig, ax = plt.subplots()
+    # lines = ax.plot(data)
+    # ax.legend(custom_lines, ['Cold', 'Medium', 'Hot'])
+
+
+
+
+
+
+
+    # substract offset
+    df['last frame'] = df['last frame'] - df['last frame'].loc[0]
+
+    # calculate relative technique distribution (relative to total time)
+    df['relative1'] = df['last frame']*100//df['last frame'].iloc[-1]
+    df['relative2'] = df['last frame'] / df['last frame'].iloc[-1]
+    df['relative3'] = round(df['last frame'] * 100 / df['last frame'].iloc[-1])
+
+    # collect all sub-techniques
+    if i == 0:
+        subtechs = list(df['sub-technique'])
+    else:
+        subtechs.extend(list(df['sub-technique']))
+    a=12
+
--- code/skripts/visualize_lucas.py
+++ code/skripts/visualize_lucas.py
@@ -2,8 +2,8 @@ import mvnx
 from pathlib import Path
 import numpy as np
 import pandas as pd
-import matplotlib.pyplot as plt
-
+import matplotlib.pyplot as pd
+from code.xski_utilites.signal import low_pass_filter
 # file path
 
 # fp = Path(r'C:\Users\b1090197\Documents\Case Study Kit\Recordings\P3\MVN\hard_round.mvnx')
@@ -23,16 +23,21 @@ fp = r'C:\Users\b1090197\Documents\Case Study Kit\Recordings\P12\MVNX\hard_round
 com = pd.read_excel(fp, sheet_name='Center of Mass', index_col=0)
 gps = pd.read_excel(fp, sheet_name='Global Position', index_col=0)
 
-plt.plot(com.iloc[:,0])
+plt.figure()
+ax = plt.axes(projection='3d')
+ax.plot3D(com['CoM pos x'], com['CoM pos y'], com['CoM pos z'])
 plt.show()
-plt.plot(com.iloc[:,1])
-plt.show()
-plt.plot(com.iloc[:,2])
-plt.show()
+plt.plot(low_pass_filter(com.iloc[:,0], 240, .125, 4))
+#plt.show()
+#plt.plot(com.iloc[:,1])
+#plt.plot(low_pass_filter(com.iloc[:,0], 240, .125, 4), low_pass_filter(com.iloc[:,2], 240, .125, 4))
+#plt.show()
+
 plt.plot(gps.iloc[:,0], gps.iloc[:,1])
 plt.show()
 #plt.plot(np.linalg.norm(gps.iloc[:,:2], axis =1), gps.iloc[:,2])
 #plt.show()
 a=12
+
 
 
