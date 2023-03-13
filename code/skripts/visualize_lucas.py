ed871d650c0da4329919a00f3606c1fc2e10c88c
LH <lucas.hoeschler@plus.ac.at>
2023-03-13 10:11:04 +0100

    added some papers + started subtech-classification

--- code/skripts/visualize_lucas.py
+++ code/skripts/visualize_lucas.py
@@ -10,4 +10,12 @@ fp = Path(r'C:\Users\b1090197\Documents\Case Study Kit\Recordings\P6\MVNX\medium
 
 data = mvnx.load(fp)
 
-com = pd.DataFrame(data.centerOfMass, columns = )
+com = pd.DataFrame(data.centerOfMass)
+com.iloc[11295:11962,:].plot()
+plt.show()
+
+com1 = np.diff(com, n=1, axis=0)
+com1 = np.gradient(com, axis=0)
+com2 = np.diff(com1, n=1, axis=0)
+
+
